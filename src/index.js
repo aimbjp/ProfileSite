7403afba87fc86798887907bac0620c9962efb46
aimbjp <aimbjpw@gmail.com>
2022-09-01 15:53:33 +0300

    start

---
+++ src/index.js
@@ -0,0 +1,22 @@
+
+
+// let weather = prompt('Rain or no?');
+
+// if(weather == 'rain'){
+//     console.log('Umb')
+// } else {
+//     console.log('Sunglusses')
+// }
+
+function FindName(){
+    let name = prompt('What\'s your name?');
+    console.log(name);
+    return(name);
+}
+
+// let name = FindName();
+// alert(`Hi, ${name}!`);
+
+const sum = (a, b) => a + b;
+
+alert(sum(10,1));
